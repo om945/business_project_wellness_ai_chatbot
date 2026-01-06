a99d7191214dea299aa030eaa0d15ca8e56de61a
om945 <ombelekar21@gmail.com>
2026-01-06 21:51:18 +0530

    Add flutter_dotenv dependency for environment variable management, load .env file in main.dart, and update Env class to retrieve API keys from dotenv

--- lib/models/env.dart
+++ lib/models/env.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Env {
-  static final String api_1 = const String.fromEnvironment('Wellnex_AI_1');
-  static final String api_2 = const String.fromEnvironment('Wellnex_AI_2');
+  static String get api_1 => dotenv.env['Wellnex_AI_1'] ?? '';
+  static String get api_2 => dotenv.env['Wellnex_AI_2'] ?? '';
 }
